4c6205076e6c41fa4df0b20a410747bc44bfcfb3
Emil Skogsund <skogix@gmail.com>
2020-02-07 14:55:10 +0100

    Init

---
+++ Assets/Graph.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph : MonoBehaviour
+{
+	public Transform pointPrefab;
+
+	private void Awake()
+	{
+		for (int i = 0; i < 10; i++)
+		{
+			Transform point = Instantiate(pointPrefab);
+			point.localPosition = Vector3.right * i;
+		}		
+	}
+}
